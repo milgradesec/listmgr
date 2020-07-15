ecb85d8e33fa32c549f2ee783de9a3ca6618dfe1
milgradesec <milgradesec@gmail.com>
2020-07-15 21:33:47 +0200

    fix

--- src/test/java/com/paesa/listmgr/AppTest.java
+++ src/test/java/com/paesa/listmgr/AppTest.java
@@ -1,8 +1,10 @@
 package com.paesa.listmgr;
 
+import org.junit.Test;
+
 public class AppTest {
-    /*@Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }*/
+    @Test public void testAppHasAGreeting() {
+        final App classUnderTest = new App();
+        //assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
 }

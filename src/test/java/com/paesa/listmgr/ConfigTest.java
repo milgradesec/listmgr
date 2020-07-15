44c7ab432e73f89380464694e3ebb5b4660ee2ce
milgradesec <milgradesec@gmail.com>
2020-07-15 22:38:27 +0200

    Add testing

--- src/test/java/com/paesa/listmgr/ConfigTest.java
+++ src/test/java/com/paesa/listmgr/ConfigTest.java
@@ -1,5 +1,15 @@
 package com.paesa.listmgr;
 
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
 public class ConfigTest {
-    
+    @Test
+    public void testRead() {
+        final ArrayList<String> lists = Config.read("lists.conf");
+        assertNotEquals(lists.size(), 0);
+    }
 }

77d2358a9f154cb4213db823cf4c8e5b64e74e4e
milgradesec <milgradesec@gmail.com>
2020-07-16 00:41:22 +0200

    Add testing

---
+++ src/test/java/com/paesa/listmgr/ParserTest.java
@@ -0,0 +1,16 @@
+package com.paesa.listmgr;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class ParserTest {
+    @Test
+    public void testGenerate() {
+        final ArrayList<String> lists = new ArrayList<String>();
+        lists.add("https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts");
+        
+        final Parser parser = new Parser(lists);
+        parser.generate();
+    }
+}

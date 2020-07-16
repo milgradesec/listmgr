5a9a0534ca5f6d8a3edb6336c295f2ee83287f33
milgradesec <milgradesec@gmail.com>
2020-07-16 02:19:05 +0200

    set as public

--- src/main/java/com/paesa/listmgr/Args.java
+++ src/main/java/com/paesa/listmgr/Args.java
@@ -4,10 +4,10 @@ import com.beust.jcommander.Parameter;
 
 public class Args {
     @Parameter(names = { "--config", "-c" })
-    String config = "lists.conf";
+    public String config = "lists.conf";
 
     @Parameter(names = { "--output", "-o" })
-    String output = "blocklist.list";
+    public String output = "blocklist.list";
 
     @Parameter(names = "--help", help = true)
     private boolean help;

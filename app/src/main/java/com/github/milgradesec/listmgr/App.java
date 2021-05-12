c9258c17579aba2663e098189dcf46cf29ec8051
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-05-12 16:26:43 +0200

    New CLI lib

--- app/src/main/java/com/github/milgradesec/listmgr/App.java
+++ app/src/main/java/com/github/milgradesec/listmgr/App.java
@@ -2,15 +2,35 @@ package com.github.milgradesec.listmgr;
 
 import java.util.ArrayList;
 
-import com.beust.jcommander.JCommander;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class App {
 
-    public static void main(final String[] argv) {
-        Args args = new Args();
-        JCommander.newBuilder().addObject(args).build().parse(argv);
+    public static void main(final String[] argv) throws ParseException {
+        Options options = new Options();
+        options.addOption("c", "config", true, "File with lists to parse");
+        options.addOption("o", "output", true, "Output file");
+        options.addOption("h", "help", false, "Print this message");
 
-        ArrayList<String> lists = Configuration.read(args.config);
+        CommandLineParser cmdParser = new DefaultParser();
+        CommandLine cmd = cmdParser.parse(options, argv);
+
+        String configFile = "lists.conf";
+        String outputFile = "blocklist.list";
+
+        if (cmd.hasOption("config")) {
+            configFile = cmd.getOptionValue("config");
+        }
+
+        if (cmd.hasOption("output")) {
+            outputFile = cmd.getOptionValue("output");
+        }
+
+        ArrayList<String> lists = Configuration.read(configFile);
         if (lists.isEmpty()) {
             return;
         }
@@ -21,7 +41,8 @@ public class App {
             int size = parser.parse(data);
             System.out.printf("Added %d domains from [%s]\n", size, list);
         }
-        parser.writeToFile(args.output);
+
+        parser.writeToFile(outputFile);
         System.out.printf("Total list size: %d\n", parser.list.size());
     }
 }

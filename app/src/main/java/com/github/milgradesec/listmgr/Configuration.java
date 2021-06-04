b9cccf8932ab5f4ea917c38785e36eec55dc151d
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-06-04 21:59:03 +0200

    Update Configuration.java

--- app/src/main/java/com/github/milgradesec/listmgr/Configuration.java
+++ app/src/main/java/com/github/milgradesec/listmgr/Configuration.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class Configuration {
 
     public static ArrayList<String> read(final String file) {
-        ArrayList<String> sources = new ArrayList<String>();
+        ArrayList<String> sources = new ArrayList<>();
 
         try {
             ArrayList<String> lines = new ArrayList<>(Files.readAllLines(Paths.get(file)));

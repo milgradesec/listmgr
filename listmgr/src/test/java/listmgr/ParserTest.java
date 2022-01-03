ca9c675330980c5b296e34b1b5ea2235a5f66913
milgradesec <38557680+milgradesec@users.noreply.github.com>
2022-01-03 16:37:56 +0100

    add unit testing for Parser

---
+++ listmgr/src/test/java/listmgr/ParserTest.java
@@ -0,0 +1,24 @@
+package listmgr;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.github.milgradesec.listmgr.Parser;
+
+import org.junit.Test;
+
+public class ParserTest {
+         
+    @Test
+    public void testParse() throws IOException {
+        Parser parser = new Parser();
+
+        String testdata = Files.readString(Paths.get("src/test/resources/testdata.txt"));
+        parser.parse(testdata);
+
+        assertEquals(4, parser.list.size());
+    }
+}
